c37e06e0a1bf2971e8e90618c1cfb37b7567f9f0
root <root@localhost.localdomain>
2026-08-26 16:00:52 +0500

    Fix: Add google() repository to dependencyResolutionManagement for splashscreen dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
+        google()
         mavenCentral()
         maven("https://dl.google.com/dl/android/maven2/")
         maven("https://maven.mozilla.org/maven2/")
